364e50785df71b525e7568b35c443403a2ec1c0d
majid163ss <majid163g@gmail.com>
2026-09-12 10:27:59 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -896,10 +896,7 @@ async def admin_match_callback(callback):
 
     match_id = int(callback.data.split(":")[-1])
 
-    async with Session() as session:
-        result = await session.execute(
-        result = await session.execute(
-
+        async with Session() as session:
         result = await session.execute(
             select(Match).where(
                 Match.id == match_id
