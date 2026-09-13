b26de6abfb660ea61118f4cd0081766184187cc0
majid163ss <majid163g@gmail.com>
2026-09-13 21:55:47 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4368,6 +4368,18 @@ async def ef_edit_content_video_handler(message: Message):
     await message.answer(
         "✅ ویدیو و کپشن با موفقیت ویرایش شد! 🎬🔥"
     )
+    async def load_admins():
+    ADMIN_IDS.clear()
+
+    async with Session() as session:
+        result = await session.execute(
+            select(Admin.telegram_id)
+        )
+
+        admin_ids = result.scalars().all()
+
+    ADMIN_IDS.update(admin_ids)
+    ADMIN_IDS.update(SUPER_ADMIN_IDS)
 async def main():
 
     await init_db()
