f7a8b91068b1c3c77ad5afa3bb3be8c859df9b81
majid163ss <majid163g@gmail.com>
2026-09-14 23:28:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4099,6 +4099,12 @@ async def prediction_handler(message: Message):
     lambda message: message.from_user.id in pending_giveaway
 )
 async def giveaway_message_handler(message: Message):
+        print(
+        f"🎲 GIVEAWAY MESSAGE | "
+        f"user={message.from_user.id} | "
+        f"text={message.text!r} | "
+        f"pending={pending_giveaway.get(message.from_user.id)}"
+    )
 
     user_id = message.from_user.id
 
